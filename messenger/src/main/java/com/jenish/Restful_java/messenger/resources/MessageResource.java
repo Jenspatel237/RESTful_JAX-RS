92f8184a6d94199006154c16b9c04a01f6350548
jenspatel237 <jenspatel237@yahoo.com>
2020-09-03 17:38:15 -0400

    Filtration

--- messenger/src/main/java/com/jenish/Restful_java/messenger/resources/MessageResource.java
+++ messenger/src/main/java/com/jenish/Restful_java/messenger/resources/MessageResource.java
@@ -2,6 +2,7 @@ package com.jenish.Restful_java.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -10,11 +11,13 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.tomcat.util.security.MD5Encoder;
 
 import com.jenish.Restful_java.messenger.model.Message;
+import com.jenish.Restful_java.messenger.resources.beans.MessageFilterBean;
 import com.jenish.Restful_java.messenger.service.MessageService;
 
 @Path("/messages")
@@ -26,8 +29,13 @@ public class MessageResource
 	MessageService messageService = new MessageService();
 	
 	@GET
-	public List<Message> getMessages()
+	public List<Message> getMessages(@BeanParam MessageFilterBean filterBean)
 	{
+		if(filterBean.getYear() > 0)
+			return messageService.getAllMessagesForYear(filterBean.getYear());
+		if(filterBean.getStart() >= 0 && filterBean.getSize() > 0)
+			return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
+		
 		return messageService.getAllMessages();
 	}
 	
